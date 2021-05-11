f8d89cfb633a92317a1809dd81a8fdece01132e8
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-05-12 03:19:16 +0900

    feat: 調理方法の詳細が表示されるように

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,28 @@
 <template>
   <v-row no-gutters>
-    <v-col v-for="recipe in recipes" :key="recipe.id" cols="12" sm="6" md="4">
-      <v-card hover tile outlined @click="handleClick(recipe)">
-        <v-card-title class="headline">{{ omitString(recipe.title, 16) }}</v-card-title>
+    <v-dialog v-model="showDialog" width="800px" transition="dialog-bottom-transition">
+      <v-card>
+        <v-card-title>{{ recipe.title }}</v-card-title>
         <v-card-text>
-          <v-img :src="recipe.imageUrl" max-height="200" contain />
-          <v-rating background-color="grey lighten-2" color="warning" size="24" :value="recipe.rate" readonly />
-          <p>{{ omitString(recipe.impression, 18) }}</p>
+          <v-rating :value="recipe.rate" background-color="grey lighten-2" color="warning" readonly size="24" />
+        </v-card-text>
+        <v-img :src="recipe.imageUrl" max-height="300" contain />
+        <v-card-title>感想</v-card-title>
+        <v-card-text>{{ recipe.impression }}</v-card-text>
+        <v-card-title>レシピ</v-card-title>
+        <v-card-text>{{ recipe.recipe }}</v-card-text>
+        <v-card-actions class="justify-end">
+          <v-btn text @click="handleCloseDialog">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-col v-for="item in recipes" :key="item.id" cols="12" sm="6" md="4">
+      <v-card hover tile outlined @click="handleClick(item)">
+        <v-card-title class="headline">{{ omitString(item.title, 16) }}</v-card-title>
+        <v-card-text>
+          <v-img :src="item.imageUrl" max-height="200" contain />
+          <v-rating background-color="grey lighten-2" color="warning" size="24" :value="item.rate" readonly />
+          <p>{{ omitString(item.impression, 18) }}</p>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
@@ -17,13 +33,29 @@
 </template>
 
 <script lang="ts">
-import { computed, ComputedRef, defineComponent, onMounted, SetupContext } from '@nuxtjs/composition-api'
+import { computed, ComputedRef, defineComponent, onMounted, reactive, ref, SetupContext } from '@nuxtjs/composition-api'
 import { RecipeStore } from '~/store'
 import { IRecipe } from '~/types/store'
 
 export default defineComponent({
   setup(_, { root }: SetupContext) {
     const store = root.$store
+
+    const initialRecipe: IRecipe = {
+      id: '',
+      title: '',
+      impression: '',
+      recipe: '',
+      rate: 0,
+      imageUrl: '',
+      createdBy: '',
+      updatedBy: '',
+      createdAt: 0,
+      updatedAt: 0,
+    }
+
+    const showDialog = ref<boolean>(false)
+    const recipe = ref<IRecipe>({ ...initialRecipe })
 
     const recipes: ComputedRef<IRecipe[]> = computed(() => store.getters['recipe/getRecipes'])
 
@@ -41,14 +73,23 @@ export default defineComponent({
       }
     }
 
-    const handleClick = (recipe: IRecipe) => {
-      console.log('debug', 'click', recipe)
+    const handleClick = (item: IRecipe) => {
+      recipe.value = { ...item }
+      showDialog.value = true
+    }
+
+    const handleCloseDialog = () => {
+      showDialog.value = false
+      recipe.value = { ...initialRecipe }
     }
 
     return {
+      recipe,
       recipes,
       omitString,
+      showDialog,
       handleClick,
+      handleCloseDialog,
     }
   },
 })
